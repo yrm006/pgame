64094cee0f92ba3b8a646d4d04ee5a2bb0fcc2d1
yrm <yrm@na-s.jp>
2024-12-12 16:30:53 +0900

    20241205

--- pgame.c
+++ pgame.c
@@ -17,6 +17,24 @@ void Teki_ugoki_yarareta(struct Teki* this){
     this->y = this->y + 1;
 }
 
+int Teki_atattaka(struct Teki* this, Vector2 hero_xy, int hero_r){
+    if( CheckCollisionCircles((Vector2){this->x+30, this->y+30}, 30, hero_xy, hero_r) ){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+void Teki_yarareta(struct Teki* this, Texture2D yarare_tx){
+    this->hp = this->hp - 1;
+    if(this->hp == 0){
+        this->tx = yarare_tx;
+        this->ugoki = Teki_ugoki_yarareta;
+    }
+}
+
+
+
 int main(){
     InitWindow(800, 450, "Ore-sama no GAME");
     SetTargetFPS(60);
@@ -27,10 +45,10 @@ int main(){
     Texture2D teki = LoadTexture("teki.png");
     Texture2D teki180 = LoadTexture("teki180.png");
 
-    int x = 400;
-    int xv = 0;
-    int y = 450 - 30;
-    int yv = 0;
+    float x = 400;
+    float xv = 0;
+    float y = 450 - 30;
+    float yv = 0;
     Color col = YELLOW;
 
     // int teki_hp = 3;
@@ -41,18 +59,43 @@ int main(){
         teki0.tx = teki;
         teki0.ugoki = Teki_ugoki_normal;
     }
+    struct Teki teki1;{
+        teki1.x = 550;
+        teki1.y = 450-60;
+        teki1.hp = 3;
+        teki1.tx = teki;
+        teki1.ugoki = Teki_ugoki_normal;
+    }
+    struct Teki teki2;{
+        teki2.x = 600;
+        teki2.y = 450-60;
+        teki2.hp = 5;
+        teki2.tx = teki;
+        teki2.ugoki = Teki_ugoki_normal;
+    }
+
+    int teki0_timer = 60*5;
+    int teki1_timer = 60*8;
 
     while(! WindowShouldClose() ){
         if(IsKeyPressed(KEY_SPACE)){
-            if(y == (450-60)){
+            if( (450-60)-1 <= y ){
                 yv = -20;
             }
         }
         if(IsKeyDown(KEY_RIGHT)){
-            xv = xv + 3;
+            if( (450-60) <= y ){
+                xv = xv + 3;
+            }else{
+                xv = xv + 0.4;
+            }
         }
         if(IsKeyDown(KEY_LEFT)){
-            xv = xv - 3;
+            if( (450-60) <= y ){
+                xv = xv - 3;
+            }else{
+                xv = xv - 0.4;
+            }
         }
 
         x = x + xv;
@@ -74,7 +117,28 @@ int main(){
             yv = yv + 1;
         }
 
+        if( !--teki0_timer ){
+            teki0.x = 500;
+            teki0.y = 450-60;
+            teki0.hp = 3;
+            teki0.tx = teki;
+            teki0.ugoki = Teki_ugoki_normal;
+
+            teki0_timer = 60*5;
+        }
+        if( !--teki1_timer ){
+            teki1.x = 550;
+            teki1.y = 450-60;
+            teki1.hp = 3;
+            teki1.tx = teki;
+            teki1.ugoki = Teki_ugoki_normal;
+
+            teki1_timer = 60*8;
+        }
+
         teki0.ugoki(&teki0);
+        teki1.ugoki(&teki1);
+        teki2.ugoki(&teki2);
 
         if( ColorToInt(col) == ColorToInt(YELLOW) ){
             col = RED;
@@ -82,14 +146,20 @@ int main(){
             col = YELLOW;
         }
 
-        if(CheckCollisionCircles((Vector2){x+30, y+30}, 30, (Vector2){teki0.x+30, teki0.y+30}, 30)){
-            // tekini atatta toki
+        if( Teki_atattaka(&teki0, (Vector2){x+30, y+30}, 30) ){
+            y = y - 30;
             yv = -20;
-            teki0.hp = teki0.hp - 1;
-            if(teki0.hp == 0){
-                teki0.tx = teki180;
-                teki0.ugoki = Teki_ugoki_yarareta;   
-            }
+            Teki_yarareta(&teki0, teki180);
+        }
+        if( Teki_atattaka(&teki1, (Vector2){x+30, y+30}, 30) ){
+            y = y - 30;
+            yv = -20;
+            Teki_yarareta(&teki1, teki180);
+        }
+        if( Teki_atattaka(&teki2, (Vector2){x+30, y+30}, 30) ){
+            y = y - 30;
+            yv = -20;
+            Teki_yarareta(&teki2, teki180);
         }
 
         BeginDrawing();
@@ -100,6 +170,8 @@ int main(){
             // DrawCircle(x, y, 30, col);
             DrawTexture(hero, x, y, WHITE);
             DrawTextureEx(teki0.tx, (Vector2){teki0.x, teki0.y}, 0, 1, WHITE);
+            DrawTextureEx(teki1.tx, (Vector2){teki1.x, teki1.y}, 0, 1, WHITE);
+            DrawTextureEx(teki2.tx, (Vector2){teki2.x, teki2.y}, 0, 1, WHITE);
         EndDrawing();
     }
 
